52bfcd367e985c0bd13fab7e8ea6014fc8de0ddf
Darkhonbek <darhonbek@gmail.com>
2019-11-13 23:35:26 -0500

    Initial Commit

---
+++ Text Recognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Text Recognizer
+//
+//  Created by Darkhonbek Mamataliev on 13/11/19.
+//  Copyright © 2019 Darkhonbek Mamataliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
