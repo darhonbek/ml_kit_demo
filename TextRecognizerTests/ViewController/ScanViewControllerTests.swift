b63e52db471bfc42db7a2d3ebfc9a34bce528dc7
Darkhonbek <darhonbek@gmail.com>
2019-11-18 01:39:49 -0500

    Add unit tests target.

---
+++ TextRecognizerTests/ViewController/ScanViewControllerTests.swift
@@ -0,0 +1,9 @@
+//
+//  ScanViewControllerTests.swift
+//  TextRecognizerTests
+//
+//  Created by Darkhonbek Mamataliev on 18/11/19.
+//  Copyright © 2019 Darkhonbek Mamataliev. All rights reserved.
+//
+
+import Foundation
---
+++ TextRecognizerTests/ViewModel/ScanViewModelTests.swift
@@ -0,0 +1,9 @@
+//
+//  ScanViewModelTests.swift
+//  TextRecognizerTests
+//
+//  Created by Darkhonbek Mamataliev on 18/11/19.
+//  Copyright © 2019 Darkhonbek Mamataliev. All rights reserved.
+//
+
+import Foundation
